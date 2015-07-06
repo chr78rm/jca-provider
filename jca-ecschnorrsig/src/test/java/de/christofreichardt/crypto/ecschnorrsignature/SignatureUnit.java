09ba849368216a60ec53a1920e925f8be9917906
chr78rm <chr78rm@onlinehome.de>
2015-07-06 17:17:17 +0200

    Several test cases related to the EC Schnorr signature algorithm added.

--- jca-ecschnorrsig/src/test/java/de/christofreichardt/crypto/ecschnorrsignature/SignatureUnit.java
+++ jca-ecschnorrsig/src/test/java/de/christofreichardt/crypto/ecschnorrsignature/SignatureUnit.java
@@ -1,96 +1,56 @@
 package de.christofreichardt.crypto.ecschnorrsignature;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 import java.util.Properties;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
+import de.christofreichardt.crypto.BaseSignatureUnit;
+import de.christofreichardt.crypto.Provider;
 import de.christofreichardt.diagnosis.AbstractTracer;
 import de.christofreichardt.diagnosis.Traceable;
-import de.christofreichardt.diagnosis.TracerFactory;
 
-public class SignatureUnit implements Traceable {
-  final private Properties properties;
-  private byte[] msgBytes;
+public class SignatureUnit extends BaseSignatureUnit implements Traceable {
 
   public SignatureUnit(Properties properties) {
-    this.properties = properties;
+    super(properties, new Provider());
+    this.keyPairAlgorithmName = "ECSchnorrSignature";
+    this.signatureAlgorithmName = "ECSchnorrSignatureWithSHA256";
   }
-  
-  @Before
-  public void init() throws IOException {
-    AbstractTracer tracer = getCurrentTracer();
-    tracer.entry("void", this, "init()");
-    
-    try {
-      this.msgBytes = loadMsgBytes();
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-  
-  private byte[] loadMsgBytes() throws IOException {
-    AbstractTracer tracer = getCurrentTracer();
-    tracer.entry("byte[]", this, "loadMsgBytes()");
-    
-    try {
-      File blindTextFile = new File(this.properties.getProperty("de.christofreichardt.crypto.schnorrsignature.SignatureUnit.blindtext", 
-          "../data/loremipsum.txt"));
-      byte[] bytes = Files.readAllBytes(blindTextFile.toPath());
-      traceMsgBytes(bytes);
-      return bytes;
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-  
-  private void traceMsgBytes(byte[] bytes) {
-    AbstractTracer tracer = getCurrentTracer();
-    for (int i=0; i<bytes.length; i++) {
-      if (i % 16 == 0) {
-        if (i != 0)
-          tracer.out().println();
-        tracer.out().printIndentString();
-      }
-      tracer.out().printf("%3d ", bytes[i] & 255);
-    }
-    tracer.out().println();
-  }
-  
   
   @Test
-  public void plainUse() {
+  public void plainUse() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
     AbstractTracer tracer = getCurrentTracer();
     tracer.entry("void", this, "plainUse()");
     
     try {
+      SignatureWithSHA256 signatureWithSHA256 = new SignatureWithSHA256();
+      KeyPairGenerator keyPairGenerator = new KeyPairGenerator();
+      KeyPair keyPair = keyPairGenerator.generateKeyPair();
+      
+      signatureWithSHA256.engineInitSign(keyPair.getPrivate());
+      signatureWithSHA256.engineUpdate(this.msgBytes, 0, this.msgBytes.length);
+      byte[] signatureBytes = signatureWithSHA256.engineSign();
+      
+      signatureWithSHA256.engineInitVerify(keyPair.getPublic());
+      signatureWithSHA256.engineUpdate(this.msgBytes, 0, this.msgBytes.length);
+      boolean verified = signatureWithSHA256.engineVerify(signatureBytes);
+      
+      Assert.assertTrue("Expected a valid signature.", verified);
+      
+      signatureWithSHA256.engineInitVerify(keyPair.getPublic());
+      signatureWithSHA256.engineUpdate(this.spoiledMsgBytes, 0, this.spoiledMsgBytes.length);
+      verified = signatureWithSHA256.engineVerify(signatureBytes);
+      
+      Assert.assertTrue("Expected an invalid signature.", !verified);
     }
     finally {
       tracer.wayout();
     }
   }
   
-  @After
-  public void exit() {
-    AbstractTracer tracer = getCurrentTracer();
-    tracer.entry("void", this, "exit()");
-    
-    try {
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-
-  @Override
-  public AbstractTracer getCurrentTracer() {
-    return TracerFactory.getInstance().getCurrentPoolTracer();
-  }
-
 }
