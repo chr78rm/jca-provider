14092ef3b3de4f13066fa3c597a7df84738f4e20
chr78rm <chr78rm@onlinehome.de>
2015-07-23 18:18:54 +0200

    Point addition formulas and test case for multiplication added.

--- elliptic-curve-arithmetic/src/main/scala/de/christofreichardt/scala/ellipticcurve/Montgomery.scala
+++ elliptic-curve-arithmetic/src/main/scala/de/christofreichardt/scala/ellipticcurve/Montgomery.scala
@@ -32,16 +32,93 @@ package affine {
             new Point(x, 0, this)
         }
       }
-      
+            
+      override def toString() = {
+        "Montgomery[a=" + this.a + ", b=" + this.b + ", p=" + this.p + "]"
+      }
     }
     
-    class Point(x: BigInt, y: BigInt, curve: Curve) extends AffinePoint(x,y, curve) {
+    class Point(x: BigInt, y: BigInt, curve: Curve) extends AffinePoint(x,y, curve) with Equals with Tracing {
       override def negate = new Point(this.x, (-this.y).mod(this.curve.p), this.curve)
-      override def add(point: Point): Element = null
-      override def multiply(scalar: BigInt): Element = null
+      
+      override def add(point: Point): Element = {
+        val tracer = getCurrentTracer()
+        withTracer("Element", this, "add(point: AffinePoint)") {
+          tracer.out().printfIndentln("this = %s", this)
+          tracer.out().printfIndentln("point = %s", point)
+          tracer.out().printfIndentln("point.negate = %s", point.negate)
+
+          if (this != point.negate) {
+//            val lambda =
+//              if (this != point)
+//                ((point.y - this.y) * (point.x - this.x).modInverse(this.curve.p)).mod(this.curve.p)
+//              else
+//                (((3 * this.x.modPow(2, this.curve.p)) + (2 * this.curve.a + this.x) + 1).mod(this.curve.p) * (2 * this.curve.b * this.y).modInverse(this.curve.p)).mod(this.curve.p)
+//            val x = (((this.curve.b * lambda.modPow(2, this.curve.p))).mod(this.curve.p) - this.curve.a - this.x - point.x).mod(this.curve.p)
+//            val y = ((lambda * (this.x - x)).mod(this.curve.p) - this.y).mod(this.curve.p)
+//            new Point(x, y, this.curve)
+            if (this != point) {
+              val xa = (this.curve.b * (point.y - this.y).modPow(2, this.curve.p)).mod(this.curve.p)
+              val xb = (point.x - this.x).modPow(2, this.curve.p)
+              val x = (xa * xb.modInverse(this.curve.p) - this.curve.a - this.x - point.x).mod(this.curve.p)
+              val ya = ((((2*this.x + point.x + this.curve.a) * (point.y - this.y)).mod(this.curve.p)) * (point.x - this.x).modInverse(this.curve.p)).mod(this.curve.p)
+              val yb = ((((this.curve.b) * ((point.y - this.y).modPow(3, this.curve.p))).mod(this.curve.p)) * (((point.x - this.x).modPow(3, this.curve.p))).modInverse(this.curve.p)).mod(this.curve.p)
+              val y = (ya - yb - this.y).mod(this.curve.p)
+              new Point(x, y, this.curve)
+            }
+            else {
+              val xa = this.curve.b * ((3 * this.x.modPow(2, this.curve.p)) + (2 * this.curve.a * this.x).mod(this.curve.p) + 1).modPow(2, this.curve.p)
+              val xb = (2 * this.curve.b * this.y).modPow(2, this.curve.p).modInverse(this.curve.p)
+              val x = ((xa * xb).mod(this.curve.p) - this.curve.a - this.x - this.x).mod(this.curve.p)
+              val ya = (((2 * this.x + this.x + this.curve.a).mod(this.curve.p)) * (((3 * this.x.modPow(2, this.curve.p)) + (2 * this.curve.a * this.x).mod(this.curve.p) + 1).mod(this.curve.p))).mod(this.curve.p)
+              val yb = (2 * this.curve.b * this.y).modInverse(this.curve.p)
+              val yc = ((this.curve.b * ((3 * this.x.modPow(2, this.curve.p)) + (2 * this.curve.a * this.x).mod(this.curve.p) + 1).modPow(3, this.curve.p))).mod(this.curve.p)
+              val yd = ((2 * this.curve.b * this.y).modPow(3, this.curve.p)).modInverse(this.curve.p)
+              val y = ((ya * yb).mod(this.curve.p) - (yc * yd).mod(this.curve.p) - this.y).mod(this.curve.p)
+              new Point(x, y, this.curve)
+            }
+          }
+          else
+            new NeutralElement
+        }
+      }
+      
+      override def multiply(scalar: BigInt): Element = Montgomery.multiplicationMethod.multiply(scalar, this)
+      
+      def canEqual(other: Any) = {
+        other.isInstanceOf[Point]
+      }
+
+      override def equals(other: Any) = {
+        other match {
+          case that: Point => that.canEqual(Point.this) && x == that.x && y == that.y
+          case _                 => false
+        }
+      }
+
+      override def hashCode() = {
+        val prime = 41
+        prime * (prime + x.hashCode) + y.hashCode
+      }
+
+      override def getCurrentTracer(): AbstractTracer = {
+        try {
+          TracerFactory.getInstance().getDefaultTracer
+        }
+        catch {
+          case ex: TracerFactory.Exception => TracerFactory.getInstance().getDefaultTracer
+        }
+      }
     }
     
     def makeCurve(coefficients: TheCoefficients, finiteField: TheFiniteField): Curve = new Curve(coefficients.a, coefficients.b, finiteField.p)
     def makePoint(coordinates: TheCoordinates, curve: TheCurve): Point = new Point(coordinates.x.mod(curve.p), coordinates.y.mod(curve.p), curve)
+
+    implicit def elemToAffinePoint(elem: Element): Point = {
+      elem match {
+        case el: NeutralElement => throw new NeutralElementException("Neutral element has been trapped.")
+        case ap: Point    => ap
+      }
+    }
   }
 }
--- elliptic-curve-arithmetic/src/test/scala/de/christofreichardt/scala/ellipticcurve/MontgomerySuite.scala
+++ elliptic-curve-arithmetic/src/test/scala/de/christofreichardt/scala/ellipticcurve/MontgomerySuite.scala
@@ -48,11 +48,29 @@ package affine {
     
     testWithTracing(this, "Random point") {
       val tracer = getCurrentTracer()
+      
       val groupLaw = Montgomery
       val curve = groupLaw.makeCurve(groupLaw.OddCharCoefficients(117050, 1), groupLaw.PrimeField(BigInt(2).pow(221) - 3))
       val randomPoint = curve.randomPoint
       
       tracer.out().printfIndentln("randomPoint = %s", randomPoint)
+    }
+    
+    testWithTracing(this, "P * order(P) = 0") {
+      val tracer = getCurrentTracer()
+      
+      val groupLaw = Montgomery
+      val curve = groupLaw.makeCurve(groupLaw.OddCharCoefficients(117050, 1), groupLaw.PrimeField(BigInt(2).pow(221) - 3))
+      val basePoint = groupLaw.makePoint(groupLaw.AffineCoordinates(4, BigInt("1630203008552496124843674615123983630541969261591546559209027208557")), curve)
+      val order = BigInt("421249166674228746791672110734682167926895081980396304944335052891")
+      val product = basePoint multiply order
+      val testPoint = groupLaw.makePoint(groupLaw.AffineCoordinates(BigInt("1606415676813498058014924151313595965130794692739589994783694077531"), 
+          BigInt("2134935430760076865959898766833545863303276772166762027696560530099")), curve)
+      
+      tracer.out().printfIndentln("curve = %s", curve)
+      tracer.out().printfIndentln("product = %s", product)
+      
+      assert(product.isNeutralElement, "Expected the NeutralElement.")
     }
   }
 }
