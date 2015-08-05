9eda84c8dd330b9f3b071383ea53f30669538a85
chr78rm <chr78rm@onlinehome.de>
2015-08-05 16:36:52 +0200

    Switched to -cp option.

--- jca-bundle/run-examples.sh
+++ jca-bundle/run-examples.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export JAVA_HOME=/opt/java/jdk1.8.0_51
-${JAVA_HOME}/bin/java -ea:de.christofreichardt.crypto.examples... -Djava.util.logging.config.file=./logging.properties -jar ./target/jca-bundle-0.0.3-SNAPSHOT.jar
+${JAVA_HOME}/bin/java -ea:de.christofreichardt.crypto.examples... -Djava.util.logging.config.file=./logging.properties -cp ./target/jca-bundle-0.0.3-SNAPSHOT.jar de.christofreichardt.crypto.examples.Main
 
