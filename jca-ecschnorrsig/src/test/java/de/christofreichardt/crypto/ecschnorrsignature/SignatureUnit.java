f802f2b687492b24ba60f0627fbd604399ee969b
chr78rm <chr78rm@onlinehome.de>
2015-08-17 18:50:49 +0200

    All BrainPool curves will be tested.

--- jca-ecschnorrsig/src/test/java/de/christofreichardt/crypto/ecschnorrsignature/SignatureUnit.java
+++ jca-ecschnorrsig/src/test/java/de/christofreichardt/crypto/ecschnorrsignature/SignatureUnit.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 
 import de.christofreichardt.crypto.BaseSignatureUnit;
 import de.christofreichardt.crypto.Provider;
+import de.christofreichardt.crypto.ecschnorrsignature.ECSchnorrSigKeyGenParameterSpec.CurveCompilation;
 import de.christofreichardt.crypto.ecschnorrsignature.ECSchnorrSigParameterSpec.PointMultiplicationStrategy;
 import de.christofreichardt.diagnosis.AbstractTracer;
 import de.christofreichardt.diagnosis.Traceable;
@@ -134,6 +135,47 @@ public class SignatureUnit extends BaseSignatureUnit implements Traceable {
       verified = verify(signature, keyPair.getPublic(), this.msgBytes, signatureBytes);
       
       Assert.assertTrue("Expected an invalid signature.", !verified);
+    }
+    finally {
+      tracer.wayout();
+    }
+  }
+  
+  @Test
+  public void customBrainPoolCurves() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, InvalidAlgorithmParameterException {
+    AbstractTracer tracer = getCurrentTracer();
+    tracer.entry("void", this, "customBrainPoolCurves()");
+    
+    try {
+      java.security.KeyPairGenerator keyPairGenerator = java.security.KeyPairGenerator.getInstance(this.keyPairAlgorithmName);
+      String[] curveIds = {"brainpoolP160r1", "brainpoolP160t1", "brainpoolP192r1", "brainpoolP192t1", "brainpoolP224r1", "brainpoolP224t1",
+          "brainpoolP256r1", "brainpoolP256t1", "brainpoolP320r1", "brainpoolP320t1", "brainpoolP384r1", "brainpoolP384t1", "brainpoolP512r1",
+          "brainpoolP512t1"};
+      for (String curveId : curveIds) {
+        keyPairGenerator.initialize(new ECSchnorrSigKeyGenParameterSpec(CurveCompilation.BRAINPOOL, curveId));
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        ECSchnorrPublicKey ecSchnorrPublicKey = (ECSchnorrPublicKey) keyPair.getPublic();
+        BigInteger order = ecSchnorrPublicKey.getEcSchnorrParams().getCurveSpec().getOrder();
+        BigInteger p = ecSchnorrPublicKey.getEcSchnorrParams().getCurveSpec().getCurve().p().bigInteger();
+        
+        tracer.out().printfIndentln("p(%d) = %s", p.bitLength(), p);
+        tracer.out().printfIndentln("order(%d) = %s", order.bitLength(), order);
+        
+        Signature signature = Signature.getInstance(this.signatureAlgorithmName);
+        byte[] signatureBytes = sign(signature, keyPair.getPrivate(), this.msgBytes);
+        boolean verified = verify(signature, keyPair.getPublic(), this.msgBytes, signatureBytes);
+        
+        Assert.assertTrue("Expected a valid signature.", verified);
+        
+        verified = verify(signature, keyPair.getPublic(), this.spoiledMsgBytes, signatureBytes);
+        
+        Assert.assertTrue("Expected an invalid signature.", !verified);
+        
+        keyPair = keyPairGenerator.generateKeyPair();
+        verified = verify(signature, keyPair.getPublic(), this.msgBytes, signatureBytes);
+        
+        Assert.assertTrue("Expected an invalid signature.", !verified);
+      }
     }
     finally {
       tracer.wayout();
