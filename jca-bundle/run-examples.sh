5ee2f36f6d8ccfb598958fd1ab10bbcf223af597
chr78rm <chr78rm@onlinehome.de>
2015-08-31 18:10:41 +0200

    Moved to 0.0.4-SNAPSHOT.

--- jca-bundle/run-examples.sh
+++ jca-bundle/run-examples.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export JAVA_HOME=/opt/java/jdk1.8.0_51
 ${JAVA_HOME}/bin/java -ea:de.christofreichardt.crypto.examples... -Djava.util.logging.config.file=./logging.properties \
--cp ./target/jca-bundle-0.0.3-SNAPSHOT.jar de.christofreichardt.crypto.examples.Main $1
+-cp ./target/jca-bundle-0.0.4-SNAPSHOT.jar de.christofreichardt.crypto.examples.Main $1
 
